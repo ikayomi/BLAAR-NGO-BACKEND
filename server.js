e13d216f248d8d61a5d5fe2ba8d5012df645de22
ikayomi <alayodavid28@gmail.com>
2026-07-24 15:15:37 +0100

    added home route

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use(express.json());
 const donateRoutes = require("./route/donate.route.js");
 const volunteerRoutes = require("./route/volunteer.route.js")
 
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Blaar NGO Backend is running 🚀",
+  });
+});
+
 app.use("/api/donate", donateRoutes);
 app.use("/api/volunteers", volunteerRoutes);
 
